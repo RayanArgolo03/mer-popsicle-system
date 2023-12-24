259209fef3b966a64adbd1de1e2de6ea6e86fe54
Rayan <rayanpetros2@gmail.com>
2023-12-24 15:16:09 -0300

    refactor: refactor data input methods

--- src/services/ResellerService.java
+++ src/services/ResellerService.java
@@ -16,30 +16,52 @@ public final class ResellerService {
 
     public ResellerService() {
         this.repository = new ResellerRepository();
+        newAccountCreated = false;
     }
 
     public boolean hasReseller() {
         return !repository.getResellers().isEmpty();
     }
 
-    //Return mok login
-    public Login receiveLogin() {
+    public boolean hasNewAccountCreated() {
+        return newAccountCreated;
+    }
 
-        String username = receiveInput("username");
+    public void toggleAccountStatus(boolean status) {
+        newAccountCreated = status;
+    }
+
+    //Mok reseller
+    public Reseller createReseller() {
+
+        String name = ReadService.readString("name");
+        if (!validName(name)) throw new ResellerException("Invalid name!");
+
+        String username = ReadService.readString("username");
         if (!validLoginInformation(username)) throw new ResellerException("Invalid username!");
 
-        String password = receiveInput("password");
+        String password = ReadService.readString("password");
+        if (!validLoginInformation(password)) throw new ResellerException("Invalid password!");
+
+        String contactPersonName = ReadService.readString("contact person name");
+        if (!validName(contactPersonName)) throw new ResellerException("Invalid contact person name!");
+
+        String phoneNumber = ReadService.readString("contact person phone number");
+        if (!validPhoneNumber(phoneNumber)) throw new ResellerException("Invalid phone number!");
+
+        return new Reseller(name, new Login(username, password),
+                new ContactPerson(name, phoneNumber));
+    }
+
+    public Login receiveLogin() {
+
+        String username = ReadService.readString("username");
+        if (!validLoginInformation(username)) throw new ResellerException("Invalid username!");
+
+        String password = ReadService.readString("password");
         if (!validLoginInformation(password)) throw new ResellerException("Invalid password!");
 
         return new Login(username, password);
-    }
-
-    public void logged(Reseller reseller) {
-        reseller.setOnline();
-    }
-
-    public void unlogged(Reseller reseller) {
-        reseller.setOffline();
     }
 
     public boolean addReseller(Reseller reseller) {
@@ -54,26 +76,8 @@ public final class ResellerService {
         return repository.findByLogin(login);
     }
 
-    //Mok reseller
-    public Reseller createReseller() {
-
-        String name = receiveInput("name");
-        if (!validName(name)) throw new ResellerException("Invalid name!");
-
-        String username = receiveInput("username");
-        if (!validLoginInformation(username)) throw new ResellerException("Invalid username!");
-
-        String password = receiveInput("password");
-        if (!validLoginInformation(password)) throw new ResellerException("Invalid password!");
-
-        String contactPersonName = receiveInput("contact person Name");
-        if (!validName(contactPersonName)) throw new ResellerException("Invalid contact person name!");
-
-        String phoneNumber = receiveInput("contact person phone number");
-        if (!validPhoneNumber(phoneNumber)) throw new ResellerException("Invalid phone number!");
-
-        return new Reseller(name, new Login(username, password),
-                new ContactPerson(name, phoneNumber));
+    public Reseller getLastResellerAdded() {
+        return repository.getLastReseller();
     }
 
     public void updateReseller(Reseller reseller) {
@@ -85,7 +89,7 @@ public final class ResellerService {
     }
 
     private void updateName(Reseller reseller) {
-        String name = receiveInput("name");
+        String name = ReadService.readString("name");
         if (!validName(name, reseller.getName()))
             throw new ResellerException("Name is the same as the old name or uses illegal characters!");
         reseller.setName(name);
@@ -93,10 +97,10 @@ public final class ResellerService {
 
     private void updateContact(Reseller reseller) {
 
-        String name = receiveInput("name");
+        String name = ReadService.readString("name");
         if (!validName(name)) throw new ResellerException("Invalid name!");
 
-        String phoneNumber = receiveInput("phone number");
+        String phoneNumber = ReadService.readString("phone number");
         if (!validPhoneNumber(phoneNumber)) throw new ResellerException("Invalid name!");
 
         ContactPerson contactPerson = new ContactPerson(name, phoneNumber);
@@ -106,41 +110,33 @@ public final class ResellerService {
         reseller.setContactPerson(contactPerson);
     }
 
-    private String receiveInput(String title) {
-        System.out.println("Enter with " + title + ":");
-        return ReadService.readString();
-    }
-
-    //Use in update
-    private boolean validName(String name, String oldName) {
-        return !areEqual(name, oldName)
-                && validName(name);
-    }
-
     //Generic method for compare to equals objects
     private <T> boolean areEqual(T t1, T t2) {
         return t1.equals(t2);
     }
 
-    //Use in  create
+
     private boolean validName(String name) {
         //Generic condition
         return name.length() > 2;
     }
 
+    private boolean validLoginInformation(String information) {
+        return information.length() > 3;
+    }
 
-    //Use to create
+    private boolean validName(String name, String oldName) {
+        return !areEqual(name, oldName)
+                && validName(name);
+    }
+
+
     private boolean validPhoneNumber(String phoneNumber) {
         return phoneNumber.matches("\\(?(\\b[0-9]{2,3}|0((x|[0-9]){2,3}[0-9]{2}))\\)?\\s*[0-9]{4,5}[- ]*[0-9]{4}\\b");
     }
 
     private boolean validNewContactPerson(ContactPerson contactPerson, ContactPerson oldContactPerson) {
         return !areEqual(contactPerson, oldContactPerson);
-    }
-
-
-    private boolean validLoginInformation(String information) {
-        return information.length() > 3;
     }
 
 
